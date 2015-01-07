9503d9e6a67752b3fab61b58bac315c6bcaeeb78
bailleul.mike@gmail.com <bailleul.mike@gmail.com@df18b61c-3bf2-06ad-1127-5184cddacdf8>
2015-01-07 17:17:39 +0000

    Test Builder Handler done

--- TestESBQ/src/com/insa/tp3g1/esbsimulator/test/TestXML.java
+++ TestESBQ/src/com/insa/tp3g1/esbsimulator/test/TestXML.java
@@ -22,6 +22,9 @@ public class TestXML {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+            boolean validationOk = false;
+            
+            try {
 		ProcessingTime processingTime = new ProcessingTime(15, "ms");
 		DataExchangeSize data = new DataExchangeSize(10, "byte");
 
@@ -31,37 +34,43 @@ public class TestXML {
 		
 		ArrayList<Provider> providers = new ArrayList<Provider>();
 		providers.add(new Provider(1, processingTime, data));
+                providers.add(new Provider(2, processingTime, data));
 
 		//test build
 		File xmlFile = new File("ScenarBuild.xml");
-		Scenario scenario = new Scenario(1, providers, 2, "scenario", consumers);
-        
-            try {
+		Scenario scenario;
+            
+                scenario = new Scenario(1, providers, 2, "scenario", consumers);
+            
                 BuilderHandler.createXmlFileFromObject(xmlFile, scenario);
+                
+                //test validate and parse
+		File xsdFile = new File("scenario.xsd");
+		
+                try {
+                    validationOk = ValidatorHandler.isXmlValidAgainstXsd(xmlFile, xsdFile);
+                } catch (SAXException ex) {
+                    System.out.println("Error: SAX exception - " + ex.getMessage());
+                } catch (IOException ex) {
+                    System.out.println("Error: IO exception - " + ex.getMessage());
+                }
             } catch (FileNotFoundException ex) {
                 System.out.print("Error: File not found");
                 //ex.printStackTrace();
             } catch (JAXBException ex) {
                 System.out.print("Error: JAXB exception - " + ex.getMessage());
-               //ex.printStackTrace();
-            } 
-           	
-		//test validate and parse
-		File xsdFile = new File("scenario.xsd");
-		boolean validationOk = false;
-           
-            try {
-                validationOk = ValidatorHandler.isXmlValidAgainstXsd(xmlFile, xsdFile);
-            } catch (SAXException ex) {
-                System.out.println("Error: SAX exception - " + ex.getMessage());
-            } catch (IOException ex) {
-                System.out.println("Error: IO exception - " + ex.getMessage());
+               //ex.printStackTrace() 
+            } catch (Exception ex) {
+                Logger.getLogger(TestXML.class.getName()).log(Level.SEVERE, null, ex);
             }
+           	
+		
             
 		
             if(validationOk) {
                     System.out.println("Validation OK");
                 try {
+                    File xmlFile = new File("ScenarBuild.xml");
                     Scenario scCreated = (Scenario) ParserHandler.getInstanceFromXmlFile(xmlFile, Scenario.class);
                 } catch (JAXBException ex) {
                     System.out.println("Error: JAXB exception - " + ex.getMessage());
